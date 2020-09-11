372a7d44a27ab5e9372528f91869ffbd02a40f10
Stephen Fink <stephenfink8@gmail.com>
2020-09-11 15:58:08 -0500

    this is the first upload

---
+++ script.js
@@ -0,0 +1,35 @@
+//over all use depends on site for users input
+//list of functions at min.
+
+//What is shown:
+    //buttons or click events for functions
+    //depeneding on function: then it gives to user
+
+//Able to log in:
+    //use the jetemail api with ajax
+    //use a storage for the user log in and password
+    //able to sigh/ donate to
+    //needs some sort of money donate functions with i.e. paypal/w.e. enbedded
+    
+//Layout functions:
+    //Owner should be able to with ease change the page as need be bases
+    //parts of nav bar should change to a page when done so
+    //images should be in a px size scale to ensure spacing of each topic for fitting
+    // the user should have some data saved base on their log in
+    //this data should only be seen/user by user input
+
+//Other notes:
+    //This is a draft of what possible min needs of a web page
+    //from there it varries on what needs to be done and how to do so.
+
+//Research:
+    //depending on other notes answers we will go and see how to improve it
+        //base on information given can see how to improve site and functions
+
+//apis:
+    //Depends on where hosting and what site is used for directly of functions
+    //How many and if monthly fees is base on site use
+
+//Type of JS lib use:
+    //jQuery, and maybe refactor to react later on
+    //Depends on css document and how it functions
